dfb71c681dc36ee03e9dea47813ddc9aad9fb889
myonlylaptop88-cmyk <myonlylaptop88@gmail.com>
2026-06-08 09:54:42 +0800

    Add Firebase config and authentication

---
+++ js/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBS0bw3PWt4yM-lT2NfLbvzORsZoNAPU64",
+  authDomain: "ecoswap-c212f.firebaseapp.com",
+  projectId: "ecoswap-c212f",
+  storageBucket: "ecoswap-c212f.firebasestorage.app",
+  messagingSenderId: "567088432752",
+  appId: "1:567088432752:web:927f9af907138d532956b5"
+};
